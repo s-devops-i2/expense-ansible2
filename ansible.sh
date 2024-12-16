7dabd493a2199e60ba295fe9e8876fa2996982ce
shuja <shuja.devops@gmail.com>
2024-12-16 22:15:06 +0530

    credentials get from vault

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,5 @@
 component=$1
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
-ansible-playbook -i $component-$env.shujathdevops.online, -e  role_name=$component expense-play.yml -e env=$env -e "@secrets.json"
+ansible-playbook -i $component-$env.shujathdevops.online, -e  role_name=$component expense-play.yml -e env=$env -e "@secrets.json" -e "@app.json"
+
