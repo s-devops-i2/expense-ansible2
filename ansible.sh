0a94e031df48591844ad9e62b5440474697d8a18
shuja <shuja.devops@gmail.com>
2024-12-16 20:41:07 +0530

    reading secrets

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 component=$1
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
-#ansible-playbook -i $component-dev.shujathdevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$component expense-play.yml -e env=dev
+ansible-playbook -i $component-$env.shujathdevops.online, -e  role_name=$component expense-play.yml -e env=$env -e "@secrets.json"
