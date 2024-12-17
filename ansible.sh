10492cb139a58369ad32120e94e827c88a427175
shuja <shuja.devops@gmail.com>
2024-12-17 14:00:39 +0530

    token removed

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,6 @@
 component=$1
 
-ansible-playbook get-secrets.yml -e vault_token=$vault_token
-ansible-playbook -i $component-$env.shujathdevops.online, -e  role_name=$component expense-play.yml -e env=$env -e "@secrets.json" -e "@app.json"
+#ansible-playbook get-secrets.yml -e vault_token=$vault_token
+#ansible-playbook -i $component-$env.shujathdevops.online, -e  role_name=$component expense-play.yml -e env=$env -e "@secrets.json" -e "@app.json"
 
+ansible-playbook -i $component-$env.shujathdevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$component expense-play.yml -e env=$env
