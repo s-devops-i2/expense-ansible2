21780106952ecfa07b0376b976e21c31ec94197f
shuja <shuja.devops@gmail.com>
2024-12-20 16:55:20 +0530

    deleting files

--- ansible.sh
+++ ansible.sh
@@ -2,5 +2,4 @@ component=$1
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e  role_name=$component -e env=$env
 ansible-playbook -i $component-$env.shujathdevops.online, -e  role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json' -e '@~/app.json'
-
-
+rm -f ~/secrets.json ~/app.json
