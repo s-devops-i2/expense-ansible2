281c5b32e72af06b8a6ad11c8d9e7a9028b2ee8f
shuja <shuja.devops@gmail.com>
2024-12-24 16:38:51 +0530

    enabling apps setup in ansible

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,6 @@
 component=$1
 
+rm -f ~/secrets.json ~/app.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e  role_name=$component -e env=$env
 ansible-playbook -i $component-$env.shujathdevops.online, -e  role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json' -e '@~/app.json'
 rm -f ~/secrets.json ~/app.json
