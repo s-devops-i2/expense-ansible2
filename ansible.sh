4f9f8ebc8bacd18c38f495e66e17fdbecb384a7d
shuja <shuja.devops@gmail.com>
2025-01-01 16:07:26 +0530

    updating common/single secrets file

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 component=$1
 
 rm -f ~/*.json
-ansible-playbook get-secrets.yml -e vault_token=$vault_token -e  role_name=$component -e env=$env
+ansible-playbook -vvv get-secrets.yml -e vault_token=$vault_token -e  role_name=$component -e env=$env
 exit 1
 ansible-playbook -i $component-$env.shujathdevops.online, -e  role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json'
 rm -f ~/*.json
