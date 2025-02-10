20dc494dc44db64e9929a069223f7b119ac3064d
shuja-git <shuja.devops@gmail.com>
2025-02-09 23:08:52 -0800

    DNS changed

--- ansible.sh
+++ ansible.sh
@@ -2,6 +2,6 @@ component=$1
 
 rm -f ~/*.json
 ansible-playbook -vvv get-secrets.yml -e vault_token=$vault_token -e  role_name=$component -e env=$env
-ansible-playbook -i $component-$env.shujathdevops.online, -e  role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json'
+ansible-playbook -i $component-$env.shujadevops.shop, -e  role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json'
 rm -f ~/*.json
 
