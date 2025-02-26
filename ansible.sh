2fe567aa53642561e1a628dc4d00591aeee08e9b
shuja-git <shuja.devops@gmail.com>
2025-02-26 15:46:36 +0530

    Making pipeline changes

--- ansible.sh
+++ ansible.sh
@@ -2,6 +2,9 @@ component=$1
 
 rm -f ~/*.json
 ansible-playbook -vvv get-secrets.yml -e vault_token=$vault_token -e  role_name=$component -e env=$env
-ansible-playbook -i $component-$env.shujadevops.shop, -e  role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json'
+#ansible-playbook -i $component-$env.shujadevops.shop, -e  role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json'
+
+aws ec2 describe-instances --filters Name=tag:Name,Values=$component-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
+ansible-playbook -i inv -e role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json'
 rm -f ~/*.json
 
