676c38e32e4cda6ac8615e39d72f1bc5c45911f7
shuja-git <shuja.devops@gmail.com>
2025-03-05 14:31:39 +0530

    modified env and component

--- ansible.sh
+++ ansible.sh
@@ -1,12 +1,10 @@
 component=$1
-env
-exit 1
 
 rm -f ~/*.json
 ansible-playbook -vvv get-secrets.yml -e vault_token=$vault_token -e  role_name=$component -e env=$env
 #ansible-playbook -i $component-$env.shujadevops.shop, -e  role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json'
 
-aws ec2 describe-instances --filters Name=tag:Name,Values=$env-$component Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
+aws ec2 describe-instances --filters Name=tag:Name,Values=$component-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 ansible-playbook -i inv -e role_name=$component expense-play.yml -e env=$env -e '@~/secrets.json'
 rm -f ~/*.json
 
